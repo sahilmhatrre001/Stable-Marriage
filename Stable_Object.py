0a141e95263a2b7de58bf5a86aa3fe217ac8e562
sahilmhatrre001 <Sahilmhatre001@gmail.com>
2020-03-29 01:48:22 +0530

    front page

---
+++ Stable_Object.py
@@ -0,0 +1,5 @@
+class Stable:
+    def __init__(self,name,number_of_pairs):
+        self.name = name;
+        self.number_of_pairs = number_of_pairs;
+        
---
+++ web/js/function.js
@@ -0,0 +1,5 @@
+
+function create_new_list()
+{
+    console.log("new list created");
+}
