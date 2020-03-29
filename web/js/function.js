ee20205389234d2a737dbf9b840e4f46d96c3be1
sahilmhatrre001 <Sahilmhatre001@gmail.com>
2020-03-29 13:38:45 +0530

    some ui changes

--- web/js/function.js
+++ web/js/function.js
@@ -1,5 +1,18 @@
-
-function create_new_list()
-{
-    console.log("new list created");
-}
+$(document).ready(function(){
+    $("#next_btn").click(function(){
+        var number_of_pairs = $("#txt_np").val();
+        if($.isNumeric(number_of_pairs))
+        {
+            $("#content").empty();
+            $("#content").append("<h4> Paris to Calculate :"+number_of_pairs+ "</h4><br>");
+            for(var i=0;i<number_of_pairs;i++)
+            {
+                $("#t_body").append("<tr><td><input type='text' id='p"+i+"'></td><td><input type='text' id='q"+i+"'></td></tr>");
+            }
+            document.getElementById("content_table").removeAttribute("hidden");
+        }
+        else{
+            alert("please enter number");
+        }
+    });
+});
