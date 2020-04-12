aaba9584549e7fb028659fa2ff3fdc1f84cb549c
sahilmhatrre001 <Sahilmhatre001@gmail.com>
2020-04-12 20:32:24 +0530

    added drag and drop in priority table

--- web/js/function.js
+++ web/js/function.js
@@ -30,14 +30,15 @@ $(document).ready(function(){
         $("#content").empty();
         $("#content_table").empty();
         $("#content").append("<h4> Choose Priorities </h4><br>");
+        $("#content").append(" <div class='alert alert-info'><b>Drag and Drop names to set priorities</b></div>");
         for(var i=0;i<number_of_pairs;i++)
         {
-            var append_row_boy = '<tr><td class ="table-success" id="boy'+i+'">'+boy_map[i]+'</td>';
-            var append_row_girl = '<tr><td class ="table-info" id="girl'+i+'">'+girl_map[i]+'</td>';
+            var append_row_boy = '<tr><td class ="tl" id="boy'+i+'">'+boy_map[i]+'</td>';
+            var append_row_girl = '<tr><td class ="tl" id="girl'+i+'">'+girl_map[i]+'</td>';
             for(var j=0;j<number_of_pairs;j++)
             {
-                append_row_boy += '<td id="g_b'+i+''+j+'">'+girl_map[j]+'</td>';
-                append_row_girl += '<td id="b_g'+i+''+j+'">'+boy_map[j]+'</td>';
+                append_row_boy += '<td id="g_b'+i+''+j+'" draggable = "true" ondragstart="drag(event)" ondrop="drop(event)" ondragover="allowDrop(event)">'+girl_map[j]+'</td>';
+                append_row_girl += '<td id="b_g'+i+''+j+'"  draggable = "true" ondragstart="drag(event)" ondrop="drop(event)" ondragover="allowDrop(event)">'+boy_map[j]+'</td>';
             }  
             append_row_boy += '</tr>';
             append_row_girl += '</tr>';
@@ -46,4 +47,37 @@ $(document).ready(function(){
         }
         document.getElementById("priority_table").removeAttribute("hidden");
     });
+
+    $("#ok_btn2").click(function(){
+        $("#content").empty();
+        $("#priority_table").empty();
+        $("#content").append("<h4> Result </h4><br>");
+    });
 });
+
+
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+
+function drag(ev)
+{
+    console.log("drag" + ev);
+    ev.dataTransfer.setData("text",ev.target.id);
+}
+
+function drop(ev)
+{
+    console.log("drop" + ev);
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    var id_1 = document.getElementById(data).id.substr(0,4);
+    var id_2 = ev.target.id.substr(0,4);
+    if(id_1 == id_2)
+    {
+        var data_preserve = document.getElementById(data).textContent;
+        document.getElementById(data).textContent = ev.target.textContent;
+        ev.target.textContent = data_preserve; 
+    }
+    // ev.target.appendChild(document.getElementById(data).textContent);
+}
